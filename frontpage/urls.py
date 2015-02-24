443d431de8d655d3993a9808ee72ed3798d3f4e7
dudley <dudley.burrows@gmail.com>
2015-02-24 01:01:36 +0000

    fixed frontpage url.py

--- frontpage/urls.py
+++ frontpage/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 # from frontpage import views
 
 urlpatterns = patterns('',
-	url(r'^$', 'frontpage.views.home.html'),
+	url(r'^$', 'frontpage.views.home'),
 )
