35797ef53eefc26cd70ef8eee6f69ba7d30e3c13
dudley <dudley.burrows@gmail.com>
2015-02-26 00:04:48 +0000

    added simplex-bootstrap. modified main framework

--- about/urls.py
+++ about/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
 	url(r'^$', 'about.views.about'),
 	url(r'^brians/$', 'about.views.brians'),
+	url(r'^location/$', 'about.views.location'),
 )
